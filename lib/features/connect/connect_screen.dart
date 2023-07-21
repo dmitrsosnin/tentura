2e413913db794c887ff8c48e1eafa7fcaa034da5
Postnikov Aleksander <alexandrim0@gmail.com>
2023-07-22 02:02:11 +0300

    - connect screen

--- lib/features/connect/connect_screen.dart
+++ lib/features/connect/connect_screen.dart
@@ -1,10 +1,23 @@
+import 'package:ferry/ferry.dart';
+import 'package:get_it/get_it.dart';
 import 'package:flutter/material.dart';
+import 'package:ferry_flutter/ferry_flutter.dart';
 
+import 'package:gravity/ui/widget/avatar_image.dart';
+import 'package:gravity/ui/widget/error_center_text.dart';
 import 'package:gravity/ui/widget/rating_button.dart';
-import 'package:gravity/features/beacon_search/beacon_search_screen.dart';
+import 'package:gravity/data/gql/beacon/_g/search_beacon.req.gql.dart';
+import 'package:gravity/features/beacon_details/beacon_details_screen.dart';
 
-class ConnectScreen extends StatelessWidget {
+class ConnectScreen extends StatefulWidget {
   const ConnectScreen({super.key});
+
+  @override
+  State<ConnectScreen> createState() => _ConnectScreenState();
+}
+
+class _ConnectScreenState extends State<ConnectScreen> {
+  final _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -18,38 +31,70 @@ class ConnectScreen extends StatelessWidget {
           leading: const RatingButton(),
           leadingWidth: RatingButton.width,
         ),
-        body: ListView(
+        body: Column(
           children: [
+            // Search control
             Padding(
-              // TBD: should be ralative
-              padding: const EdgeInsets.only(top: 200),
-              child: Column(
-                children: [
-                  const Text(
-                    'Nothing here yet\nFind your friends to get started!',
-                    textAlign: TextAlign.center,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  hintText: 'Type a Code (at least 3 symbols)',
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      _searchController.clear();
+                      setState(() {});
+                    },
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 20),
-                    child: FilledButton(
-                      onPressed: () => Navigator.of(context).push(
-                        MaterialPageRoute<void>(
-                          builder: (context) => const BeaconSearchScreen(),
-                        ),
-                      ),
-                      child: const Text('Search by Code'),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    child: Text(
-                      'or\nScan a QR',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ],
+                ),
+                onSubmitted: (value) {
+                  if (value.length > 2) setState(() {});
+                },
               ),
             ),
+            if (_searchController.text.length > 2)
+              Operation(
+                client: GetIt.I<Client>(),
+                operationRequest: GSearchBeaconReq(
+                  (b) => b..vars.startsWith = '${_searchController.text}%',
+                ),
+                builder: (context, response, error) {
+                  if (response?.loading ?? false) {
+                    return const Center(
+                      child: CircularProgressIndicator.adaptive(),
+                    );
+                  } else if (response?.data == null) {
+                    return ErrorCenterText(response: response, error: error);
+                  }
+                  final beacons = response!.data!.beacon;
+                  return ListView.separated(
+                    shrinkWrap: true,
+                    itemCount: beacons.length,
+                    itemBuilder: (context, i) => ListTile(
+                      leading: AvatarImage(
+                        size: 40,
+                        userId: beacons[i].author.has_picture
+                            ? beacons[i].author.id
+                            : '',
+                      ),
+                      title: Text(beacons[i].title),
+                      subtitle: Text(
+                        beacons[i].description,
+                        maxLines: 1,
+                      ),
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute<void>(
+                          builder: (context) =>
+                              BeaconDetailsScreen(beacon: beacons[i]),
+                        ),
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(20),
+                    separatorBuilder: (_, __) => const Divider(),
+                  );
+                },
+              ),
           ],
         ),
       );
