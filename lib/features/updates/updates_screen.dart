cf7c618255f22ba372967a23b212961203279055
Postnikov Aleksander <alexandrim0@gmail.com>
2023-08-18 16:11:26 +0300

    - no tab on updates screen

--- lib/features/updates/updates_screen.dart
+++ lib/features/updates/updates_screen.dart
@@ -4,29 +4,19 @@ class UpdatesScreen extends StatelessWidget {
   const UpdatesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            actions: [
-              TextButton(
-                onPressed: () {},
-                child: const Text('Mark all as read'),
-              )
-            ],
-            bottom: const TabBar(
-              tabs: [
-                Text('Updates'),
-                Text('Comments'),
-              ],
-            ),
-          ),
-          body: const TabBarView(
-            children: [
-              Center(child: Text('You don’t have more updates to review')),
-              Center(child: Text('Nothing here yet')),
-            ],
-          ),
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: const Text('Mark all as read'),
+            )
+          ],
         ),
+        body: Center(
+            child: Text(
+          'Nothing here yet',
+          style: Theme.of(context).textTheme.displaySmall,
+        )),
       );
 }
