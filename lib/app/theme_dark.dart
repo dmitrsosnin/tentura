233ef39e506e1c675f6b64e148ce6f8bf9f2e55e
Postnikov Aleksander <alexandrim0@gmail.com>
2024-04-26 17:46:41 +0300

    - fix dark theme

--- lib/app/theme_dark.dart
+++ lib/app/theme_dark.dart
@@ -1,7 +1,3 @@
 import 'package:flutter/material.dart';
 
-const _primaryColor = Color(0xFF303030);
-
-final themeDark = ThemeData.dark().copyWith(
-  colorScheme: ColorScheme.fromSeed(seedColor: _primaryColor),
-);
+final themeDark = ThemeData.dark();
