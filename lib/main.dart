9eedde28d9d0575c44c096a1e47e04f9cce4d922
Postnikov Aleksander <alexandrim0@gmail.com>
2024-05-01 16:41:01 +0300

    - update packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:url_strategy/url_strategy.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'app/app.dart';
@@ -11,7 +10,6 @@ Future<void> main() async {
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-  setPathUrlStrategy();
   await SentryFlutter.init(
     (options) => options
       ..dsn = const String.fromEnvironment('SENTRY_URL')
