c9f111065bbb6d1cfbe5f5d984d0fe05130f1faa
Postnikov Aleksander <alexandrim0@gmail.com>
2024-06-18 02:51:03 +0300

    - fetch profile on start

--- lib/features/profile/ui/bloc/profile_cubit.dart
+++ lib/features/profile/ui/bloc/profile_cubit.dart
@@ -24,6 +24,7 @@ class ProfileCubit extends Cubit<ProfileState>
         _pickImageCase = pickImageCase,
         super(ProfileState(user: User.empty)) {
     hydrate();
+    fetch();
   }
 
   ProfileCubit.dummy({
