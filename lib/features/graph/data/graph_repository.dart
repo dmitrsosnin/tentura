897f422d79f238fc4f820c37c901afd0c1baf0b1
Postnikov Aleksander <alexandrim0@gmail.com>
2024-08-13 01:52:21 +0300

    - add context selector to graph view

--- lib/features/graph/data/graph_repository.dart
+++ lib/features/graph/data/graph_repository.dart
@@ -1,3 +1,4 @@
+import 'package:tentura/domain/entity/beacon.dart';
 import 'package:tentura/data/service/remote_api_service.dart';
 
 import 'gql/_g/graph_fetch.data.gql.dart';
@@ -10,18 +11,23 @@ class GraphRepository {
 
   final RemoteApiService _remoteApiService;
 
-  Future<Iterable<GGraphFetchData_graph>> fetch({
-    required String focus,
-    required bool positiveOnly,
-    required int limit,
+  Future<(Iterable<GGraphFetchData_graph>, Beacon?)> fetch({
+    bool positiveOnly = true,
+    String? context,
+    String? focus,
+    int offset = 0,
+    int limit = 5,
   }) =>
       _remoteApiService.gqlClient
           .request(GGraphFetchReq(
             (b) => b.vars
               ..focus = focus
               ..limit = limit
+              ..offset = offset
+              ..context = context
               ..positive_only = positiveOnly,
           ))
           .firstWhere((e) => e.dataSource == DataSource.Link)
-          .then((r) => r.dataOrThrow(label: _label).graph);
+          .then((r) => r.dataOrThrow(label: _label))
+          .then((r) => (r.graph, r.beacon_by_pk as Beacon?));
 }
--- lib/features/graph/ui/screen/graph_screen.dart
+++ lib/features/graph/ui/screen/graph_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:tentura/ui/utils/ui_utils.dart';
 import 'package:tentura/ui/bloc/state_base.dart';
 
+import 'package:tentura/features/context/ui/widget/context_drop_down.dart';
+
 import '../bloc/graph_cubit.dart';
 import '../widget/graph_body.dart';
 
@@ -10,34 +12,50 @@ class GraphScreen extends StatelessWidget {
   const GraphScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          actions: [
-            PopupMenuButton(
-              itemBuilder: (context) {
-                final cubit = context.read<GraphCubit>();
-                return <PopupMenuEntry<void>>[
-                  PopupMenuItem<void>(
-                    onTap: cubit.jumpToEgo,
-                    child: const Text('Go to Ego'),
-                  ),
-                  const PopupMenuDivider(),
-                  PopupMenuItem<void>(
-                    onTap: cubit.togglePositiveOnly,
-                    child: cubit.state.positiveOnly
-                        ? const Text('Show negative')
-                        : const Text('Hide negative'),
-                  ),
-                ];
-              },
-            ),
-          ],
-          title: const Text('Graph view'),
+  Widget build(BuildContext context) {
+    final cubit = context.read<GraphCubit>();
+    return Scaffold(
+      appBar: AppBar(
+        // Menu :
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (context) {
+              return <PopupMenuEntry<void>>[
+                PopupMenuItem<void>(
+                  onTap: cubit.jumpToEgo,
+                  child: const Text('Go to Ego'),
+                ),
+                const PopupMenuDivider(),
+                PopupMenuItem<void>(
+                  onTap: cubit.togglePositiveOnly,
+                  child: cubit.state.positiveOnly
+                      ? const Text('Show negative')
+                      : const Text('Hide negative'),
+                ),
+              ];
+            },
+          ),
+        ],
+
+        // Title
+        title: const Text('Graph view'),
+
+        // Context selector
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(40),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 8),
+            child: ContextDropDown(onChanged: cubit.setContext),
+          ),
         ),
-        body: BlocListener<GraphCubit, GraphState>(
-          listenWhen: (p, c) => c.status.isFailure,
-          listener: showSnackBarError,
-          child: const GraphBody(),
-        ),
-      );
+      ),
+
+      // Graph
+      body: BlocListener<GraphCubit, GraphState>(
+        listenWhen: (p, c) => c.status.isFailure,
+        listener: showSnackBarError,
+        child: const GraphBody(),
+      ),
+    );
+  }
 }
