419f25c356256a9d9e21e1922890d64a5e4c6b28
Postnikov Aleksander <alexandrim0@gmail.com>
2024-10-03 03:49:23 +0300

    - bump version

--- lib/features/beacon/ui/screen/beacon_create_screen.dart
+++ lib/features/beacon/ui/screen/beacon_create_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:typed_data';
+import 'package:get_it/get_it.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
-import 'package:get_it/get_it.dart';
 
 import 'package:tentura/consts.dart';
 import 'package:tentura/domain/entity/geo.dart';
